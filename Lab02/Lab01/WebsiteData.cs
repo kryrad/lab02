2201f9fbdfc2408e1c724012b66ce0e3f8ca52f3
kryrad <48049544+kryrad@users.noreply.github.com>
2019-03-20 01:15:03 +0100

    Async

---
+++ Lab02/Lab01/WebsiteData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    public class WebsiteDataClass
+    {
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+}
